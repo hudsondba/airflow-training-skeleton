88af5aab9d4dd8a283a7147c5bee70db175ea2a9
Hudson Santos <hsantos@cg-mb2019027.local>
2019-11-22 10:58:37 +0100

    exercise 2

---
+++ dags/test_2.py
@@ -0,0 +1,54 @@
+from datetime import timedelta, datetime
+
+import airflow
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+
+
+args = {
+    'owner': 'Airflow',
+    'start_date': datetime(2019, 10, 5, 7)
+}
+
+dag = DAG(
+    dag_id='test_dag_2',
+    default_args=args,
+    schedule_interval='0 0 * * *',
+    dagrun_timeout=timedelta(minutes=60),
+)
+
+
+def print_current_dt():
+    print(datetime.today())
+
+
+task_pd = PythonOperator(
+    dag=dag, task_id="printcurrentdat", python_callable=print_current_dt
+)
+
+wait1 = BashOperator(
+    task_id='wait 1 ',
+    bash_command='sleep 1',
+    dag=dag,
+)
+
+wait5 = BashOperator(
+    task_id='wait 5 ',
+    bash_command='sleep 1',
+    dag=dag,
+)
+
+wait10 = BashOperator(
+    task_id='wait 10 ',
+    bash_command='sleep 1',
+    dag=dag,
+)
+
+end_t = DummyOperator(task_id="end_task", dag=dag,)
+
+
+
+task_pd >> [wait1, wait5,wait10] >> end_t
